649fe4f6075b55a685d90070351aae1df75cab2f
Mr Wayne <a4arpon@gmail.com>
2023-07-01 19:32:01 +0600

    fix: navbar fixed

---
+++ src/pages/about-me/MyExperience.jsx
@@ -0,0 +1,13 @@
+const MyExperience = () => {
+  return (
+    <div className="bg-base-300 bg-opacity-40 rounded-2xl p-3 lg:p-5">
+      <h3 className="text-3xl text-center border-b-2 pb-2">My Experience</h3>
+      <div className="mt-10 flex flex-col lg:flex-row gap-3 justify-between">
+        <a className="text-2xl">No Experience Yet</a>
+        <p>Looking For</p>
+      </div>
+    </div>
+  )
+}
+
+export default MyExperience
